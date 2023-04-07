cee3d353fa04c0645323fd7a2ad466135d15a5ad
Ravi Nagi <rasvindra@hotmail.com>
2023-04-07 12:24:39 -0700

    wrote the logic to add and get from DB

--- Develop/client/src/js/database.js
+++ Develop/client/src/js/database.js
@@ -13,9 +13,32 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  try {
+    const jateDb = await openDB("jate", 1);
+    const tx = jateDb.transaction("jate", "readwrite");
+    const store = tx.objectStore("jate");
+    const request = store.put({ jate: content });
+    const result = await request;
+    console.log(result, "#####TEST111111");
+  } catch {
+    console.log("putDb not implemented","DB not working!")
+  }
+};
+
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  try {
+    const jateDb = await openDB("jate", 1);
+    const tx = jateDb.transaction("jate", "readonly");
+    const store = tx.objectStore("jate");
+    const request = store.getALL();
+    const result = await request;
+    console.log(result, "#####TEST222222222");
+  } catch {
+    console.log("putDb not implemented","DB not working!")
+  }
+}
 
 initdb();
